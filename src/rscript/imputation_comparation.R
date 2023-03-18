01b206a9a1a64c124c87704bb93f6e9bb5d94802
Tu T. Do <tu.dothanh1906@gmail.com>
2023-03-18 11:35:43 +0700

    u: adding mean imputation:

--- src/rscript/imputation_comparation.R
+++ src/rscript/imputation_comparation.R
@@ -157,5 +157,26 @@ EM_run = function(X_train, y_train, X_test, y_test){
 	X_test_imp = tail(X_cmb_imp, n = n_test); 
 
 	return( list("train" = X_train_imp,
-				"test" = X_test_imp) )
+				"test" = X_test_imp) );
+};
 
+MeanImp_run = function(X_train, y_train, X_test, y_test) {
+	# Number of train & test sample
+	n_train = nrow(X_train);
+	n_test	= nrow(X_test);
+
+	# Combining train & test
+	X_bind = rbind(X_train, X_test);
+	
+	# Impute train data with mean
+	X_bind_imp = impute_mean(X_bind);
+
+	# Impute test data with train's mean
+	X_train_imp = head(X_bind, n=n_train);
+	X_test_imp	= tail(X_bind, n=n_test);
+
+	return(list(
+				"train"	: X_train_imp,
+				"test"	: X_test_imp
+	));
+};
